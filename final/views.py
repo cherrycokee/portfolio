6899ebcfff481b8c640d4de9821f7ce0c52db23d
portfolio <cherrycokee@naver.com>
2021-01-13 12:54:12 +0900

    django project start

--- final/views.py
+++ final/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.shortcuts import render
 # Create your views here.
+# def index(request):
+#     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+def index(request):
+    post = Post.objects.all()
+    context = {
+        'post' : post,
+    }
+    return render(request, 'final/final.html', context)
